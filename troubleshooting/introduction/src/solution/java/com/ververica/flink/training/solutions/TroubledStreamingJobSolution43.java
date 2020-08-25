fbd6e7fbd1ee75d18c1f6cf0571fb778f44eba9e
Nico Kruber <nico@ververica.com>
2020-08-25 11:40:01 +0200

    [ververica] port troubled streaming job solutions to WatermarkStrategy

--- troubleshooting/introduction/src/solution/java/com/ververica/flink/training/solutions/TroubledStreamingJobSolution43.java
+++ troubleshooting/introduction/src/solution/java/com/ververica/flink/training/solutions/TroubledStreamingJobSolution43.java
@@ -1,5 +1,6 @@
 package com.ververica.flink.training.solutions;
 
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.common.functions.RichFlatMapFunction;
 import org.apache.flink.api.java.utils.ParameterTool;
@@ -9,10 +10,8 @@ import org.apache.flink.runtime.metrics.DescriptiveStatisticsHistogram;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks;
 import org.apache.flink.streaming.api.functions.sink.DiscardingSink;
 import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
-import org.apache.flink.streaming.api.watermark.Watermark;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
@@ -26,7 +25,7 @@ import com.ververica.flink.training.common.SourceUtils;
 import com.ververica.flink.training.common.WindowedMeasurements;
 
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
 
 import static com.ververica.flink.training.common.EnvironmentUtils.createConfiguredEnvironment;
 import static com.ververica.flink.training.common.EnvironmentUtils.isLocal;
@@ -60,8 +59,11 @@ public class TroubledStreamingJobSolution43 {
 				.name("FakeKafkaSource")
 				.uid("FakeKafkaSource")
 				.assignTimestampsAndWatermarks(
-						new MeasurementTSExtractor(Time.of(250, TimeUnit.MILLISECONDS),
-								Time.of(1, TimeUnit.SECONDS)))
+						WatermarkStrategy
+								.<FakeKafkaRecord>forBoundedOutOfOrderness(Duration.ofMillis(250))
+								.withTimestampAssigner(
+										(element, timestamp) -> element.getTimestamp())
+								.withIdleness(Duration.ofSeconds(1)))
 				.name("Watermarks")
 				.uid("Watermarks")
 				.flatMap(new MeasurementDeserializer())
@@ -137,69 +139,6 @@ public class TroubledStreamingJobSolution43 {
 
 		private SimpleMeasurement deserialize(final byte[] bytes) throws IOException {
 			return instance.readValue(bytes, SimpleMeasurement.class);
-		}
-	}
-
-	public static class MeasurementTSExtractor implements
-			AssignerWithPeriodicWatermarks<FakeKafkaRecord> {
-		private static final long serialVersionUID = 2L;
-
-		private long currentMaxTimestamp;
-		private long lastEmittedWatermark = Long.MIN_VALUE;
-		private long lastRecordProcessingTime;
-
-		private final long maxOutOfOrderness;
-		private final long idleTimeout;
-
-		MeasurementTSExtractor(Time maxOutOfOrderness, Time idleTimeout) {
-			if (maxOutOfOrderness.toMilliseconds() < 0) {
-				throw new RuntimeException("Tried to set the maximum allowed " +
-						"lateness to " + maxOutOfOrderness +
-						". This parameter cannot be negative.");
-			}
-
-			if (idleTimeout.toMilliseconds() < 0) {
-				throw new RuntimeException("Tried to set the idle Timeout" + idleTimeout +
-						". This parameter cannot be negative.");
-			}
-
-			this.maxOutOfOrderness = maxOutOfOrderness.toMilliseconds();
-			this.idleTimeout = idleTimeout.toMilliseconds();
-			this.currentMaxTimestamp = Long.MIN_VALUE;
-		}
-
-		/**
-		 * Gets the configured out of orderness (in ms).
-		 */
-		public long getMaxOutOfOrdernessInMillis() {
-			return maxOutOfOrderness;
-		}
-
-		@Override
-		public final Watermark getCurrentWatermark() {
-
-			// if last record was processed more than the idleTimeout in the past, consider this
-			// source idle and set timestamp to current processing time
-			long currentProcessingTime = System.currentTimeMillis();
-			if (lastRecordProcessingTime < currentProcessingTime - idleTimeout) {
-				this.currentMaxTimestamp = currentProcessingTime;
-			}
-
-			long potentialWM = this.currentMaxTimestamp - maxOutOfOrderness;
-			if (potentialWM >= lastEmittedWatermark) {
-				lastEmittedWatermark = potentialWM;
-			}
-			return new Watermark(lastEmittedWatermark);
-		}
-
-		@Override
-		public final long extractTimestamp(FakeKafkaRecord element, long previousElementTimestamp) {
-			lastRecordProcessingTime = System.currentTimeMillis();
-			long timestamp = element.getTimestamp();
-			if (timestamp > currentMaxTimestamp) {
-				currentMaxTimestamp = timestamp;
-			}
-			return timestamp;
 		}
 	}
 
