5ee28c3e2333eb7f19f908f3dad27ae1905eb375
Nico Kruber <nico@ververica.com>
2022-04-20 12:31:38 +0200

    [FLINK-27322][gradle] spotless apply (adding license headers)

--- hourly-tips/src/solution/scala/org/apache/flink/training/solutions/hourlytips/scala/HourlyTipsSolution.scala
+++ hourly-tips/src/solution/scala/org/apache/flink/training/solutions/hourlytips/scala/HourlyTipsSolution.scala
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.flink.training.solutions.hourlytips.scala
 
 import org.apache.flink.api.common.JobExecutionResult
