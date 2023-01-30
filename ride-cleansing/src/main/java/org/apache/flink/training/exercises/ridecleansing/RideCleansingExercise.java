9b08857b489210e740d1ddb773e3edcc8f7cb40c
Swathi Gottumukkala <122466385+ness-swathigottumukkala@users.noreply.github.com>
2023-01-30 09:31:26 -0500

    Revert "Changes for the flink training exercises module 1"

--- ride-cleansing/src/main/java/org/apache/flink/training/exercises/ridecleansing/RideCleansingExercise.java
+++ ride-cleansing/src/main/java/org/apache/flink/training/exercises/ridecleansing/RideCleansingExercise.java
@@ -26,7 +26,6 @@ import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.training.exercises.common.datatypes.TaxiRide;
 import org.apache.flink.training.exercises.common.sources.TaxiRideGenerator;
-import org.apache.flink.training.exercises.common.utils.GeoUtils;
 import org.apache.flink.training.exercises.common.utils.MissingSolutionException;
 
 /**
@@ -81,8 +80,7 @@ public class RideCleansingExercise {
     public static class NYCFilter implements FilterFunction<TaxiRide> {
         @Override
         public boolean filter(TaxiRide taxiRide) throws Exception {
-            return GeoUtils.isInNYC(taxiRide.startLon, taxiRide.startLat) &&
-                    GeoUtils.isInNYC(taxiRide.endLon, taxiRide.endLat);
+            throw new MissingSolutionException();
         }
     }
 }
