18cc2c4b2a42b7526d248ef7ac979cff3dac2ba4
Glauber Sousa <gsousa@mmsi.com>
2024-05-07 09:59:06 -0300

    Lab 1 - Replace new NYCFilter() with a lambda expression

--- ride-cleansing/src/main/java/org/apache/flink/training/exercises/ridecleansing/RideCleansingExercise.java
+++ ride-cleansing/src/main/java/org/apache/flink/training/exercises/ridecleansing/RideCleansingExercise.java
@@ -70,7 +70,7 @@ public class RideCleansingExercise {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         // set up the pipeline
-        env.addSource(source).filter(new NYCFilter()).addSink(sink);
+        env.addSource(source).filter((r) -> GeoUtils.isInNYC(r.startLon, r.startLat) && GeoUtils.isInNYC(r.endLon, r.endLat)).addSink(sink);
 
         // run the pipeline and return the result
         return env.execute("Taxi Ride Cleansing");
